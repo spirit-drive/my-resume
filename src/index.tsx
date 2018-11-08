d63b9c3c1acdd7a17b9a0b372c61448944331c47
Igor Zvyagin <spirit-drive@yandex.ru>
2018-11-08 19:13:55 +0300

    start

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+const Header = () => <div>Работает!!!!!</div>;
+
+ReactDOM.render(<Header />, document.getElementById("root"));
+
+import './styles/index.styl'
